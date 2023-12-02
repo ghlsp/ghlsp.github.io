d45ab584a9e8da6751a208f3331f9bca7f791eae
ghlsp <83146077+ghlsp@users.noreply.github.com>
2023-12-02 04:05:39 +0000

    Update autodeploy.yml Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,6 @@
-var posts=["2022/10/22/API/","2022/10/06/Termux部署hexo/","2022/10/17/服务器ROOT权限密码登录/","2022/10/06/常用指令/","2022/10/06/基本语法/","2022/11/23/OneDriveUploader/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};var friend_link_list=[{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true}];
+var posts=["2022/10/22/API/","2022/11/23/OneDriveUploader/","2022/10/06/Termux部署hexo/","2022/10/06/基本语法/","2022/10/06/常用指令/","2022/10/17/服务器ROOT权限密码登录/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true}];
     var refreshNum = 1;
     function friendChainRandomTransmission() {
       const randomIndex = Math.floor(Math.random() * friend_link_list.length);
--- sh.sh
+++
@@ -1,8 +0,0 @@
-echo "Start"
-hexo clean
-git add .
-git commit -m "更新"
-git push
-echo "Finish"
-echo 按任意键继续◆
-read -n 1
